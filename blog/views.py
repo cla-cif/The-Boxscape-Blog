3057a68700455c4455a25ce588833185b6eeb695
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-06-05 18:23:15 +0000

    displaying data from models in list.html

--- blog/views.py
+++ blog/views.py
@@ -3,22 +3,22 @@ from django.views import generic
 from .models import Post
 
 
-# def post_list(request):
-#     posts = Post.published.all()
-#     return render(request,
-#                   'blog/post/list.html',
-#                   {'posts:posts'})
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts:posts'})
 
 
-# def post_detail(request, year, month, day, post):
-#     post = get_object_or_404(Post, slug=post,
-#                              status='published',
-#                              publish__year=year,
-#                              publish__month=month,
-#                              publish__day=day)
-#     return render(request,
-#                   'blog/post/detail.html',
-#                   {'post': post})
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
 
 class PostList(generic.ListView):
     model = Post
