1e2ad91606916f2894bbb7a7ee85cf271ed8bda1
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-06-08 21:15:30 +0000

    install crispy form, create form.py, edit detail.htm, BUG: form not showing

--- boxscape/settings.py
+++ boxscape/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'allauth',
     'cloudinary',
     'cloudinary_storage',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.staticfiles',
     'blog',
 ]
@@ -57,6 +59,9 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
