c0692d7166235d417c35d9f433c615e1ec6ceebe
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-06-09 20:09:18 +0000

    implement comment submit functionality, edit comment form, fix bug: form is showing

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,5 @@ from .models import Comment
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ('body',)
--- blog/views.py
+++ blog/views.py
@@ -50,3 +50,35 @@ class PostDetail(View):
                 "comment_form": CommentForm()
             },
         )
+
+    def post(self, request, slug, *args, **kwargs):
+
+        queryset = Post.objects.all()
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("created")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
