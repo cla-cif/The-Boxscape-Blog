e2e76747b970f5d0c37d7002d5115d7f3071abce
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-06-18 19:49:04 +0000

    add latest posts list to the sidebar and created its template

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -3,6 +3,13 @@ from ..models import Post
 
 register = template.Library()
 
+
 @register.simple_tag()
 def total_posts():
-    return Post-published.count()
+    return Post.objects.count()
+
+
+@register.inclusion_tag('latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.objects.order_by('-published')[:count]
+    return {'latest_posts': latest_posts}
