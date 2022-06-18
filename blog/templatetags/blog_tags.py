4c8435f57b8da8058b636736951d48bf619cf52d
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-06-18 20:14:39 +0000

    add most commented post list to sidebar

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 from ..models import Post
 
 register = template.Library()
@@ -13,3 +14,10 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.objects.order_by('-published')[:count]
     return {'latest_posts': latest_posts}
+
+
+@register.simple_tag()
+def get_most_commented_posts(count=5):
+    return Post.objects.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
