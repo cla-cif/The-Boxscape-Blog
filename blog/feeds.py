2b3d3ffac65c6a96dcd05c69bbba11343c80417a
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-07-16 21:06:19 +0000

    fix bug preventing creation of feeds

--- blog/feeds.py
+++ blog/feeds.py
@@ -1,5 +1,6 @@
 from django.contrib.syndication.views import Feed
 from django.template.defaultfilters import truncatewords
+from django.utils.feedgenerator import Atom1Feed
 from django.urls import reverse_lazy, reverse
 from .models import Post
 
@@ -10,26 +11,17 @@ class LatestPostsFeed(Feed):
     description = 'Check the new posts from The Boxscape blog'
 
     def items(self):
-        return Post.objects.filter(status='pub')[:5]
-        # return Post.objects.order_by('-published')[:5]
+        return Post.objects.filter(status='pub').order_by('-published')[:5]
 
     def item_title(self, item):
         return item.title
-
+       
     def item_description(self, item):
         return truncatewords(item.body, 30)
+ 
+    def item_link(self, item):
+        return reverse('post_detail', args=[item.slug])
 
 
-
-    # def items(self):
-    #     # return Post.objects.filter(status='pub').order_by('-published')[:5]
-    #     return Post.objects.all()
-
-    # def item_title(self, item):
-    #     return item.title
-
-    # def item_description(self, item):
-    #     return truncatewords(item.body, 30)
-
-    # def item_link(self, item):
-    #     return reverse('list.html', args=[item.pk])
+class atomFeed(Feed):
+    feed_type = Atom1Feed
