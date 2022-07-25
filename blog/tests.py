28da958a838f23471aaacda8bececb8ca677f51f
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-07-25 18:08:55 +0000

    add test. BUG not running

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from users.models import User
+from .models import Post, Comment, Author
 
-# Create your tests here.
+
+class PostTest(TestCase):
+    """Testing class for posts"""
+    def test_post_create_validation_failed(self):
+        """This test checks the validation process and message by entering an empty post"""
+        res = self.client.post('/create', {'title': '', 'body': ''})
+        assert b'This field is required' in res.content
