0c1676db5f2f4c5eb3036c6ce8e3d530065046d9
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-08-01 13:20:58 +0000

    add database for testing in settings.py

--- blog/tests.py
+++ blog/tests.py
@@ -1,36 +1,22 @@
 from django.test import TestCase, Client, SimpleTestCase
 import unittest
 from django.urls import reverse, resolve
-# from users.models import User
-from .models import Post, Comment, Author
+from .models import Post
 from .views import PostList
 
 
-class TestBlogUrls(SimpleTestCase):
+class TestModels(TestCase):
+    def test_model_str(self):
+        title = Post.objects.create(title="This is the blog title")
+        self.assertEqual(str(title), "This is the blog title")
+
+
+# class TestBlogUrls(SimpleTestCase):
     
-    def test_home_url_is_resolved(self):
-        url = reverse('home')
-        self.assertEquals(resolve(url).func.view_class, PostList)
+#     def test_home_url_is_resolved(self):
+#         url = reverse('home')
+#         self.assertEquals(resolve(url).func.view_class, PostList)
 
 
 
-# TEST NOT WORKING
-# class TestModels(TestCase):
-#     def test_model_str(self):
-#         t = Post.objects.create(title="This is the post title")
-#         a = Author.objects.create(user="Jane")
-#         c = Comment.objects.create(body="This is the comment body")
-#         n = Comment.objects.create(name="John")
-#         self.assertEqual(str(t), "This is a title")
-#         self.assertEqual(str(a), "This is an author")
-#         self.assertEqual(str(c), "This is a comment")
-#         self.assertEqual(str(n), "This is a name")
-
-# TEST NOT WORKING
-# class PostTest(TestCase):
-#     """Testing class for posts"""
-#     def test_post_create_validation_failed(self):
-#         """This test checks the validation process and message by entering an empty post"""
-#         res = self.client.post('/create', {'title': '', 'body': ''})
-#         assert b'This field is required' in res.content
 
--- boxscape/settings.py
+++ boxscape/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 import dj_database_url
 from django.contrib.messages import constants as messages
 
@@ -120,25 +121,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'boxscape.wsgi.application'
 
-# TAGGIT_TAGS_FROM_STRING = 'appname.utils.comma_splitter'
-# TAGGIT_STRING_FROM_TAGS = 'appname.utils.comma_joiner'
-
-# TAGGIT_FORCE_LOWERCASE = True NOT WORKING
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
+if 'test' in sys.argv or 'test_coverage' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
