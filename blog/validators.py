6ed9525770ce8f76c0185b1e97e4fe36c1e88a9b
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-08-04 09:48:20 +0000

    add format image to url validator for post image

--- blog/validators.py
+++ blog/validators.py
@@ -3,6 +3,6 @@ import re
 
 
 def validate_url(value):
-    reg = re.compile('(?:([^:/?#]+):)?(?://([^/?#]*))?([^?#]*\.(?:jpg|gif|png))(?:\?([^#]*))?(?:#(.*))?') # noqa
+    reg = re.compile('(?:([^:/?#]+):)?(?://([^/?#]*))?([^?#]*\.(?:jpg|jpeg|gif|png))(?:\?([^#]*))?(?:#(.*))?') # noqa
     if not reg.match(value):
         raise ValidationError('Unsupported file extension.')
