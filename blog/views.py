dc36a8f85de13d70cba49cd34fa87b9702897a34
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-08-05 15:34:49 +0000

    fix bug preventing rendering of post detail with unique tag

--- blog/views.py
+++ blog/views.py
@@ -35,7 +35,7 @@ class PostDetail(View):
         # fetch post by similarity
         post_tags_ids = post.tags.values_list('id', flat=False)
         similar_posts = Post.objects.filter(tags__in=post_tags_ids)\
-            .exclude(id=post.id)
+            # .exclude(id=post.id)
         similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
             .order_by('-same_tags')
         # like and dislike functionality
@@ -70,8 +70,7 @@ class PostDetail(View):
         number_comments = len(comments)
         # retrieving post by similarity
         post_tags_ids = post.tags.values_list('id', flat=True)
-        if post_tags_ids is None:
-            return post.tags.values_list(id=1)
+        print(post_tags_ids)
         similar_posts = Post.objects.filter(tags__in=post_tags_ids)\
             .exclude(id=post.id)
         similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
